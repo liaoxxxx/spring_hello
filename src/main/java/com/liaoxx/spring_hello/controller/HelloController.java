28a350f4df6bb077f299211d1daf430f01ca526d
liaoxx000x <993501756@qq.com>
2019-03-04 17:20:15 +0800

    get   @RequestParame
    @RequestParame

--- src/main/java/com/liaoxx/spring_hello/controller/HelloController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.liaoxx.spring_hello.controller;
 import org.apache.catalina.connector.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
@@ -11,14 +12,17 @@ import java.util.Map;
 
 @Controller
 public class HelloController {
+
+
     @ResponseBody
     @RequestMapping("/hello")
-    public String hello() {
-        return "hello world";
+    public String hello(@RequestParam String name) {
+        return "name:"+name;
     }
 
     @RequestMapping("/success")
     public String success(Map <String, Object> map){
+
         map.put("user", Arrays .asList("张三","李四","王五","赵六"));
         map.put("title","输出");
         map.put("imgUrl","/js.png");
