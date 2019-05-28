4836a8dd44d3a8e9ba10b04158d99dc43aa0cb97
liaoxx <993501756@qq.com>
2019-05-29 03:55:23 +0800

    2019-05-29  Spider Controller get all recommend video  and parse to Map Object

--- src/main/java/com/liaoxx/spring_hello/controller/SpiderController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/SpiderController.java
@@ -1,5 +1,9 @@
 package com.liaoxx.spring_hello.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -12,6 +16,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @ResponseBody
@@ -19,15 +29,31 @@ import java.io.IOException;
 public class SpiderController {
     @RequestMapping("/query")
     public Object query(){
+
         CloseableHttpClient hc= HttpClients.createDefault();
         HttpGet get=new HttpGet("https://www.bilibili.com/index/recommend.json");
         try {
+            //new
             CloseableHttpResponse response=hc.execute(get);
             HttpEntity entity =response.getEntity();
-            String string = EntityUtils.toString(entity, "utf-8");
-            System.out.println(string);
+            String jsonString = EntityUtils.toString(entity, "utf-8");
+            //System.out.println(jsonString);
+            //关闭请求
             response.close();
             hc.close();
+            //json 解析
+            Map map=JSON.parseObject(jsonString); //转为map
+            Object listObj =map.get("list");  //取 key => list
+            List listParse=(JSONArray) listObj; //转为
+            List<Map> recommendList=new ArrayList<>();
+            for (Object object :listParse){
+                recommendList.add( (Map) object);
+            }
+            for (Map map1 :recommendList){
+                System.out.println(map1.get("pic"));
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
