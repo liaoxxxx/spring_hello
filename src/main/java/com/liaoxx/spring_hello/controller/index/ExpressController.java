aacc6bc2c1a12908d3279dd01592653afe55d7c8
liaoxx <993501756@qq.com>
2019-06-19 03:47:19 +0800

    2019-06-18 kuaiDiNiaoAPI Express

--- src/main/java/com/liaoxx/spring_hello/controller/index/ExpressController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/index/ExpressController.java
@@ -1,6 +1,7 @@
 package com.liaoxx.spring_hello.controller.index;
 
 
+import com.liaoxx.spring_hello.service.KdniaoTrackQueryAPI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,9 @@ public class ExpressController {
     public String query( HttpServletRequest request){
        if( request.getMethod().equals("POST")){
           String expressNo=  request.getParameter("expressNo");
+           System.out.println(expressNo);
+           KdniaoTrackQueryAPI kdn=  new KdniaoTrackQueryAPI();
+           System.out.println( kdn.getClass());
        }
         return "./index/express/query";
 
--- src/test/java/com/liaoxx/spring_hello/SpringHelloApplicationTests.java
+++ src/test/java/com/liaoxx/spring_hello/SpringHelloApplicationTests.java
@@ -2,24 +2,39 @@ package com.liaoxx.spring_hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.io.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringHelloApplicationTests {
-    @Autowired
-    DataSource dataSource;
+
     @Test
-    public void contextLoads() {
-        //System.out.println("打印dataSource,getClass-----------------------");
-        System.out.println(dataSource.getClass());
-        //System.out.println("打印dataSource,getClass-----------------------");
+    public void main() throws  IOException{
+        File f = new File("a.txt");
+        FileOutputStream fop = new FileOutputStream(f);
+        // 构建FileOutputStream对象,文件不存在会自动新建
+
+        OutputStreamWriter writer = new OutputStreamWriter(fop, "UTF-8");
+        // 构建OutputStreamWriter对象,参数可以指定编码,默认为操作系统默认编码,windows上是gbk
+
+        writer.append("中文输入");
+        // 写入到缓冲区
+
+        writer.append("\r\n");
+        // 换行
+
+        writer.append("English");
+        // 刷新缓存冲,写入到文件,如果下面已经没有写入的内容了,直接close也会写入
+        writer.append("hello world");
+        writer.close();
+        // 关闭写入流,同时会把缓冲区内容写入文件,所以上面的注释掉
+
+        fop.close();
+        // 关闭输出流,释放系统资源
+
+
     }
 
 }
