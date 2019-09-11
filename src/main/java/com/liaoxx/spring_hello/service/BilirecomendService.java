bd13bc03f88c1027001ccceb2e6613e00c1543d9
liaoxx <993501756@qq.com>
2019-09-12 04:07:35 +0800

    2019-09-12  @CachePut and @Cacheable

--- src/main/java/com/liaoxx/spring_hello/service/BilirecomendService.java
+++ src/main/java/com/liaoxx/spring_hello/service/BilirecomendService.java
@@ -3,10 +3,12 @@ package com.liaoxx.spring_hello.service;
 import com.liaoxx.spring_hello.entity.spider.BiliRecommendEntity;
 import com.liaoxx.spring_hello.repository.BiliRecommendVideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.OneToMany;
+import java.util.Map;
 
 @Service
 public class BilirecomendService {
@@ -16,5 +18,17 @@ public class BilirecomendService {
     @Cacheable(cacheNames = "BiliRecommend",key="#id")
     public BiliRecommendEntity getBiliRcmdById(int id){
         return biliRecommendVideoRepository.getOne(id);
+    }
+
+
+    public void insertBiliRcmd(){
+
+    }
+    @CachePut(cacheNames = "BiliRecommend",key = "#id")
+    public BiliRecommendEntity updateBilirecmd(int id, Map<String,Object> map){
+        BiliRecommendEntity biliRecommendEntity=biliRecommendVideoRepository.getOne(id);
+
+        biliRecommendVideoRepository.save(biliRecommendEntity);
+        return biliRecommendEntity;
     }
 }
