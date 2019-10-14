237408fb91230f9849e0411fce7e23497095d0a3
liaohasee <993501756@qq.com>
2019-10-14 18:02:19 +0800

    2019-10-14 redis config

--- src/main/java/com/liaoxx/spring_hello/controller/admin/AdminController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/admin/AdminController.java
@@ -3,13 +3,11 @@ package com.liaoxx.spring_hello.controller.admin;
 import com.liaoxx.spring_hello.entity.Admin;
 import com.liaoxx.spring_hello.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -31,7 +29,6 @@ public class AdminController {
         return "/admin/manager/login.html";     //返回了视图页面
     }
 
-    @CachePut
     @RequestMapping("/e")
     public String queryUser(Map map) {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from  user where id >=355 and id<=358");
