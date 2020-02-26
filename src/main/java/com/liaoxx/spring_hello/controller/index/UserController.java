7eb54f434637281e708626923aff815c22a4d3cd
liaoxx <993501756@qq.com>
2020-02-27 01:14:49 +0800

    2020-02-26 update application properties

--- src/main/java/com/liaoxx/spring_hello/controller/index/UserController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/index/UserController.java
@@ -2,14 +2,18 @@ package com.liaoxx.spring_hello.controller.index;
 
 import com.liaoxx.spring_hello.entity.User;
 import com.liaoxx.spring_hello.mapper.UserMapper;
+import com.liaoxx.spring_hello.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    //@Autowired
-    //private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @RequestMapping("/add")
     public String addUser(){
@@ -19,6 +23,16 @@ public class UserController {
        // user.setPasswd("liao993501756");
         //user.setSalt("adwed");
         //userMapper.install(user);
+        return "success";
+    }
+
+    @RequestMapping("/list")
+    public String list(){
+        List<UserModel> userModelList= userMapper.getAll();
+        for (UserModel userIte: userModelList) {
+            System.out.println(userIte.toString());
+        }
+        System.out.println();
         return "success";
     }
 }
--- src/main/java/com/liaoxx/spring_hello/entity/User.java
+++ src/main/java/com/liaoxx/spring_hello/entity/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
-
+@Table(name="sys_user")
 public class User  implements Serializable {
     @Id
     @GeneratedValue
