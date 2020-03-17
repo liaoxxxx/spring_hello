370eeb7b4aaf09faa0b88e9e62e6ab98d22eea00
liaoxx <993501756@qq.com>
2020-03-18 01:07:12 +0800

    2020-03-18  nothing

--- src/main/java/com/liaoxx/spring_hello/controller/index/UserController.java
+++ src/main/java/com/liaoxx/spring_hello/controller/index/UserController.java
@@ -1,10 +1,16 @@
 package com.liaoxx.spring_hello.controller.index;
 
+import com.liaoxx.spring_hello.component.Audience;
+import com.liaoxx.spring_hello.dto.UserDto;
+import com.liaoxx.spring_hello.entity.SysRole;
 import com.liaoxx.spring_hello.mapper.UserAddressMapper;
 import com.liaoxx.spring_hello.mapper.UserMapper;
 import com.liaoxx.spring_hello.model.UserModel;
 import com.liaoxx.spring_hello.service.UserService;
+import com.liaoxx.spring_hello.util.Base64Util;
 import com.liaoxx.spring_hello.util.JsonResponse;
+import com.liaoxx.spring_hello.util.JwtTokenUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +31,12 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    Audience audience;
+
+    @Autowired
+    Base64Util base64Util;
+
     @RequestMapping("/list")
     public String list(){
         List<UserModel> userModelList= userMapper.getAll();
@@ -36,9 +48,13 @@ public class UserController {
     @ResponseBody
     @RequestMapping("/login")
     public Map<String, Object> login(@RequestParam(value = "username" ,required = true) String username, Map<String, Object> map){
-        UserModel user= userMapper.getByUserName(username);
-        System.out.println(user.toString());
-        map.put("user",user);
+        UserModel user= userService.findByUsername(username);
+        String token =JwtTokenUtil.createUserJWT(user,audience);
+        UserDto userDto=new UserDto();
+        BeanUtils.copyProperties(user,userDto);
+        map.put("token",token);
+        map.put("timestamp", System.currentTimeMillis());
+        map.put("user",userDto);
         return JsonResponse.Success("success",map) ;
     }
 }
